abc1e00ba908878785eb7eda83b1b806adfd4975
llincooi <33573023+llincooi@users.noreply.github.com>
2020-06-23 23:33:47 +0800

    Update MakeMovie2.m

--- make_movie/fucntionalized/MakeMovie2.m
+++ make_movie/fucntionalized/MakeMovie2.m
@@ -1,6 +1,6 @@
 clear all;
-G_HMM = [2.5 3 4.3 4.5 5.3 6.3 6.5 7.5 9 12 20];
-G_OU = [2.5 3 4.3 4.5 5.3 6.3 6.5 7.5 9 12 20];%list of Gamma value
+G_HMM = [2.5 4.3 4.5 7.5 20];
+G_OU = [4.3 7.5];%list of Gamma value
 calibration_date = '20200219';
 HMM_time =5;%Time length of HMM of movie
 OU_time = 5;%Time length of OU of movie
@@ -8,11 +8,11 @@ makemovie_folder = 'C:\Users\hydro_leo\Documents\GitHub\retina_exp\make_movie\fu
 date = '0609';
 seed_date = '0421';
 mean_lumin =6.5;
-movie_folder = '\\192.168.0.100\Experiment\Retina\2020Videos\0609v\videos\';
+movie_folder = 'G:\0609v\videos\';
 cd(movie_folder)
 mkdir HMM
 mkdir OU
-videoworkspace_folder = '\\192.168.0.100\Experiment\Retina\2020Videos\0609v\videoworkspace\';
+videoworkspace_folder = 'G:\0609v\videoworkspace\';
 cd(makemovie_folder);
 
 % G_HMM = [4.3];
@@ -32,13 +32,13 @@ makeOLED_Bar_video2(makemovie_folder, 3*pi/4, 'UR_DL', movie_folder, videoworksp
 makeOLED_Bar_video2(makemovie_folder, pi/2, 'UD', movie_folder, videoworkspace_folder,'cSTA', seed_date,date,calibration_date,5,[2.5],mean_lumin,-1,1);
 makeOLED_Bar_video2(makemovie_folder, pi/4, 'UL_DR', movie_folder, videoworkspace_folder,'cSTA', seed_date,date,calibration_date,5,[2.5],mean_lumin,-1,1);
 
-%% Smooth OU Bright part
+%% Smooth OU Bright
 makeOLED_Bar_video2(makemovie_folder, 0, 'RL', [movie_folder,'OU'], videoworkspace_folder,'OUsmooth', seed_date,date,calibration_date,OU_time,G_OU,mean_lumin,1,1);
 makeOLED_Bar_video2(makemovie_folder, pi/2, 'UD', [movie_folder,'OU'], videoworkspace_folder,'OUsmooth', seed_date,date,calibration_date,OU_time,G_OU,mean_lumin,1,1);
 makeOLED_Bar_video2(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'OU'], videoworkspace_folder,'OUsmooth', seed_date,date,calibration_date,OU_time,G_OU,mean_lumin,1,1);
 makeOLED_Bar_video2(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'OU'], videoworkspace_folder,'OUsmooth', seed_date,date,calibration_date,OU_time,G_OU,mean_lumin,1,1);
 
-%% Smooth OU Dark part
+%% Smooth OU Dark
 makeOLED_Bar_video2(makemovie_folder, 0, 'RL', [movie_folder,'OU'], videoworkspace_folder,'OUsmooth', seed_date,date,calibration_date,OU_time,G_OU,mean_lumin,-1,1);
 makeOLED_Bar_video2(makemovie_folder, pi/2, 'UD', [movie_folder,'OU'], videoworkspace_folder,'OUsmooth', seed_date,date,calibration_date,OU_time,G_OU,mean_lumin,-1,1);
 makeOLED_Bar_video2(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'OU'], videoworkspace_folder,'OUsmooth', seed_date,date,calibration_date,OU_time,G_OU,mean_lumin,-1,1);
@@ -110,12 +110,12 @@ makeOLED_Bar_video_part(makemovie_folder, pi/4, 'UL_DR', [movie_folder,'OU'], vi
 makeOLED_Bar_video_part(makemovie_folder, 3*pi/4, 'UR_DL', [movie_folder,'OU'], videoworkspace_folder,'OUsmooth', seed_date,date,calibration_date,OU_time,G_OU,mean_lumin,j,1,'right')
 
 %% Grating
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0618',calibration_date, 6.5, 13)
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0618',calibration_date, 0, 13)
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0618',calibration_date, 0, 6.5)
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0617',calibration_date, 6.5, 13)
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0617',calibration_date, 0, 13)
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0617',calibration_date, 0, 6.5)
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0619',calibration_date, 6.5, 13)
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0619',calibration_date, 0, 13)
-makeGratingvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0619',calibration_date, 0, 6.5)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0618',calibration_date, 6.5, 13)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0618',calibration_date, 0, 13)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0618',calibration_date, 0, 6.5)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0617',calibration_date, 6.5, 13)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0617',calibration_date, 0, 13)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0617',calibration_date, 0, 6.5)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0619',calibration_date, 6.5, 13)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0619',calibration_date, 0, 13)
+makeGrating_shuffledvideo(makemovie_folder,movie_folder, videoworkspace_folder, '0619',calibration_date, 0, 6.5)
