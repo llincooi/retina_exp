bb046531d454e11d6f68cbc16b0f11db307f25f6
llincooi <33573023+llincooi@users.noreply.github.com>
2021-07-01 21:05:13 +0800

    dither

--- make_movie/fucntionalized/transform_matrix_bm.m
+++ make_movie/fucntionalized/transform_matrix_bm.m
@@ -1,6 +1,5 @@
-function img = transform_matrix_bm(calibration_date,all_bar,degree)
+function img = transform_matrix_bm(calibration_date,all_bar,degree, maxlum, minlum)
 %    length(all_bar) should eqauls to diaganol of diplaying region 
-   
     load(['C:\retina_makemovie\calibration\',calibration_date,'oled_calibration\calibration.mat'])
     load(['C:\retina_makemovie\calibration\',calibration_date,'oled_calibration\oled_boundary_set.mat']);
     
@@ -12,6 +11,16 @@ function img = transform_matrix_bm(calibration_date,all_bar,degree)
 
     index = (ceil(region_bm/2)-(region-1)/2):(ceil(region_bm/2)+(region-1)/2);
     rotate_all_bars = imrotate(all_bars,degree,'nearest','crop');
+    if length(minlum) ==1
+        rotate_all_bars = rotate_all_bars*(maxlum-minlum)+minlum;
+    else
+        lowestbackground_lumin = minlum(1);
+        ditherratio_lumin = minlum(0);
+        background = rand(size(rotate_all_bars));
+        background(background>=ditherratio) = 1;
+        background(background<ditherratio) = 0;
+        rotate_all_bars = rotate_all_bars*bar_lumin+(1-rotate_all_bars).*background*lowestbackground_lumin;
+    end
 %     imagesc(rotate_all_bars(index, index));
     
     img=zeros(screen_y,screen_x);%full screen pixel matrix %it's the LED screen size
